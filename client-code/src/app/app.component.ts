13874dcce46f8caf9bd48f19aaf7b819c22e9d68
Wolfgang Jentner <wolfgang.jentner@uni-konstanz.de>
2019-05-10 18:38:47 +0200

    refactoring of layout

--- client-code/src/app/app.component.ts
+++ client-code/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Episode } from './episode-vis/episode';
+import { TimelineOptions } from './timeline-vis/timeline-options';
 
 @Component({
   selector: 'dbvis-root',
@@ -12,7 +13,12 @@ export class AppComponent implements OnInit {
 
   episodeData: Episode;
 
+  timelineOptions: TimelineOptions = {
+    begin: new Date('2020-01-01 00:00:00'),
+    end: new Date('2020-03-31 23:59:59')
+  };
+
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    // throw new Error('Method not implemented.');
   }
 }
