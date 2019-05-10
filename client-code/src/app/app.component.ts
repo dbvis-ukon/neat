ed6e63beb21a3b4f4d1b097f0fcea56db962e521
Wolfgang Jentner <wolfgang.jentner@uni-konstanz.de>
2019-05-10 13:25:35 +0200

    added episode component for rita

--- client-code/src/app/app.component.ts
+++ client-code/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Episode } from './episode-vis/episode';
 
 @Component({
   selector: 'dbvis-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'vast-challenge-gc19';
+
+  episodeData: Episode = {
+    episode: 'test',
+    bla: 'test'
+  } as Episode;
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 }
