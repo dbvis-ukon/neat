0deafec414adbe79ab11e07de6a333bb064dc19c
Wolfgang Jentner <wolfgang.jentner@uni-konstanz.de>
2019-05-10 19:33:56 +0200

    map vis component

--- client-code/src/app/app.component.ts
+++ client-code/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Episode } from './episode-vis/episode';
 import { TimelineOptions } from './timeline-vis/timeline-options';
+import { MapData } from './map-vis/map-data';
+import { MatSliderChange } from '@angular/material';
 
 @Component({
   selector: 'dbvis-root',
@@ -18,7 +20,31 @@ export class AppComponent implements OnInit {
     end: new Date('2020-03-31 23:59:59')
   };
 
+  mapData: MapData[];
+
   ngOnInit(): void {
     // throw new Error('Method not implemented.');
+  }
+
+  /**
+   * on every slider change this function is triggered
+   * @param change the change event
+   */
+  sliderChanged(change: MatSliderChange) {
+    this.mapData = this.generateRandomMapData(change.value);
+  }
+
+  private generateRandomMapData(n): MapData[] {
+    const arr: MapData[] = [];
+
+    for (let i = 0; i < n; i++) {
+      arr.push({
+        x: Math.random(),
+        y: Math.random(),
+        r: Math.random()
+      });
+    }
+
+    return arr;
   }
 }
--- client-code/src/app/app.module.ts
+++ client-code/src/app/app.module.ts
@@ -1,24 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AngularResizedEventModule } from 'angular-resize-event';
+import { MatSliderModule, MatButtonModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EpisodeVisComponent } from './episode-vis/episode-vis.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TimelineVisComponent } from './timeline-vis/timeline-vis.component';
+import { MapVisComponent } from './map-vis/map-vis.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EpisodeVisComponent,
-    TimelineVisComponent
+    TimelineVisComponent,
+    MapVisComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    AngularResizedEventModule
+    AngularResizedEventModule,
+    MatSliderModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
