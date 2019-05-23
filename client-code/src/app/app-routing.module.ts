a638c4e020643336b714b3625105636d10e149ba
Wolfgang Jentner <wolfgang.jentner@uni-konstanz.de>
2019-05-23 17:58:59 +0200

    [#9] add route for component
    Former-commit-id: a5b705f2cf41f647415447820ee9550ddfbb2bd0

--- client-code/src/app/app-routing.module.ts
+++ client-code/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EchoTestComponent } from './echo-test/echo-test.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'echotest',
+  component: EchoTestComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
